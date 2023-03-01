e10a44eb6b6cc7256d60ac0d0d2042db22b5ed3e
dariovp <dariovp01@gmail.com>
2023-02-28 22:14:06 -0300

    fix: test configuration

--- packages/aeonium-server/src/services/users.service.test.ts
+++ packages/aeonium-server/src/services/users.service.test.ts
@@ -1,17 +1,18 @@
 import tap from 'tap';
 import User from 'user';
-import userService from "../services/user.service";
-
-tap.pass('This is ok!');
+import userService from "./user.service";
 
 tap.test('User Service: Get User By ID', async childtest => {
   let user: User | null = await userService.getById(1);
 
   childtest.strictSame(user, {
     "id": 1,
+    "pfp": "1",
     "name": "James",
     "bio": "",
     "followers": [],
     "following": []
   })
 });
+
+// tap.test('User Service: getById() returns data')
